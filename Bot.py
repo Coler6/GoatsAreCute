4a4dff083cbe9dc73065e3b4ba235d1e9e55652a
Coler6gamer <67660562+Coler6gamer@users.noreply.github.com>
2020-07-06 13:05:46 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,28 +1,77 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from itertools import cycle
 import random
+import asyncio
 import os
 client = commands.Bot(command_prefix = 'g!')
+
+class BotData:
+    def __init__(self):
+        self.welcome_channel = None
+        self.goodbye_channel = None
+
+botdata = BotData()
 
 @client.event
 async def on_ready():
     print('Bot is ready')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="goats         g!help for help."))
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} has joined the server, here we **goat** again.')
-    await on_member_join.send(f'{member} has joined the server, here we **goat** again.')
+    if botdata.welcome_channel != None:
+        await botdata.welcome_channel.send(f"{member.mention} has just joined the server, here we **goat** again! :smile: ")
+
+    else:
+        print("Welcome channel was not set.")
+
 @client.event
 async def on_member_remove(member):
-    print(f'{member} has left the server, you have **goat** to be **kid**ding me. That is too **bad**')
-    await on_member_remove.send(f'{member} has left the server, you have **goat** to be **kid**ding me. That is too **bad**')
+    if botdata.goodbye_channel != None:
+        await botdata.goodbye_channel.send(f"{member.mention} has left the server, you have **goat** to be **kid**ding me. That is too **bad**. :frowning: ")
+
+    else:
+        print("Goodbye channel was not set.")
+
 @client.command()
-async def illumix(ctx):
-    await ctx.send('https://pbs.twimg.com/profile_images/1146134369674809349/WjI4mMZc_400x400.png')
+async def set_welcome_channel(ctx, channel_name=None):
+    if channel_name != None:
+        for channel in ctx.guild.channels:
+            if channel.name == channel_name:
+                botdata.welcome_channel = channel
+                await ctx.channel.send(f"Welcome channel has been set to: {channel.name}")
+                await channel.send("This is the new welcome channel!")
+
+    else:
+        await ctx.channel.send("You must need a welcome channel for this command to work.")
+
+@client.command()
+async def set_goodbye_channel(ctx, channel_name=None):
+    if channel_name != None:
+        for channel in ctx.guild.channels:
+            if channel.name == channel_name:
+                botdata.goodbye_channel = channel
+                await ctx.channel.send(f"Goodbye channel has been set to: {channel.name}")
+                await channel.send("This is the new goodbye channel!")
+
+    else:
+        await ctx.channel.send("You must need a goodbye channel for this command to work.")
+
+
+@client.command()
+async def goat(ctx):
+    await ctx.send('https://i.imgur.com/GaPoxOD.jpg')
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command()
+async def goats(ctx):
+    responses = ["https://i.imgur.com/GaPoxOD.jpg",
+                "https://i.imgur.com/Sb7aaeA.jpg"]
+    await ctx.send(f'Goated! {random.choice(responses)}')
 
 @client.command(aliases=['8ball', 'test'])
 async def _8ball(ctx, *, question):
@@ -74,13 +123,5 @@ async def unban(ctx, *, member):
             await ctx.guild.unban(user)
             await ctx.send(f'Unbanned {user.mention} better not be a pain in the **horn**')
             return
-
-@commands.command(name='unban')
-@commands.guild_only()
-@commands.has_permissions(ban_members=True)
-async def unban(self, ctx, userId):
-    user = discord.Object(id=userId)
-    await ctx.guild.unban(user)
-    await ctx.send(f"Unbanned {user}")
 
 client.run('NzI3MjYwOTE4NTA0ODgyMjU2.XvpTzQ.mFmynFz6q6_7bMEcUDaY_xOoLb0')
